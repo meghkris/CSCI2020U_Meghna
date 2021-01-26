0c3b567cddd45ed9129eef32c4d79436be848d3b
Meghna Krishnan <meghna.krishnan@uoit.net>
2021-01-26 01:40:07 -0500

    Made a change to source file in master branch

--- lab01/Test.java
+++ lab01/Test.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is a test file.");
+        System.out.println("These are changes made in the master branch.");
     }
 }
